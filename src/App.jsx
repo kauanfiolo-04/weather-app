cf53b099907a3cfec04aec9ff645e6ad6f0a6db2
kauanfiolo-04 <kauanfiolo@hotmail.com>
2023-01-04 16:17:04 -0300

    estilizando a checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,54 @@ import initialData from './helpers/initialData'
 import Card from './components/Card/Card'
 
 function App() {
-  const [city, setCity]=useState('')
-  const [data, setData]=useState(initialData)
-  const [checked,setChecked]=useState(false)
+  const [city, setCity] = useState('')
+  const [data, setData] = useState(initialData)
+  const [checked, setChecked] = useState(false)
 
-
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault() //impede de recarregar apos o submit
 
-    fetchData(city).then((res)=>{
-      setData(res)
-    })
+      fetchData(city).then((res) => {
+        setData(res)
+      })
   }
 
-  const handleCheck=()=>{
+  const handleCheck = () => {
     setChecked(!checked)
   }
 
   return (
     <div className="flex flex-col w2-full h-screen items-center sm:justify-center p-4">
-      <form onSubmit={ handleSubmit } className='fixed bottom-0 w-full flex p-4 sm:relative justify-center'>
-        <input 
-          type='text'
-          placeholder='City' 
-          className='p-2 rounded-lg outline-none w-full sm:max-w-[300px] flex-1'
+      <form
+        onSubmit={handleSubmit}
+        className="fixed bottom-0 w-full flex p-4 sm:relative justify-center"
+      >
+        <input
+          type="text"
+          placeholder="City"
+          className="p-2 rounded-lg outline-none w-full sm:max-w-[300px] flex-1"
           value={city}
-          onChange={({ target: { value } })=> setCity(value)}
+          onChange={({ target: { value } }) => setCity(value)}
         />
-        <button 
+        <button
           type="submit"
-          className='bg-cyan-800 p-2 rounded-lg ml-3 text-white font-bold'
+          className="bg-cyan-800 p-2 rounded-lg ml-3 text-white font-bold"
         >
           Search
         </button>
       </form>
 
-      <div>
-        <input type="checkbox" name='u' className='' onChange={ handleCheck } />
-        <span>Lorem</span>
+      <div className='flex items-center jutify-center space-x-1 text-sky-600 font-bold text-md mt-4'>
+        <input
+          type="checkbox"
+          name="temperature unit"
+          className=''
+          onChange={ handleCheck } 
+        />
+        <span>Check if you want to change the unit to Fahrenheit!</span>
       </div>
-      
-      <Card data={data} unit={checked ? 'F' : 'C'}/>
 
+      <Card data={data} unit={checked ? 'F' : 'C'} />
     </div>
   )
 }
--- src/helpers/initialData.js
+++ src/helpers/initialData.js
@@ -1,44 +1,43 @@
-const initialData={
-
-  "location": {
-      "name": "__",
-      "region": "__",
-      "country": "__",
-      "lat": -22.9,
-      "lon": -47.08,
-      "tz_id": "America/Sao_Paulo",
-      "localtime_epoch": 1670887461,
-      "localtime": "2022-12-12 20:24"
+const initialData = {
+  location: {
+    name: '__',
+    region: '__',
+    country: '__',
+    lat: -22.9,
+    lon: -47.08,
+    tz_id: 'America/Sao_Paulo',
+    localtime_epoch: 1670887461,
+    localtime: '2022-12-12 20:24',
   },
-  "current": {
-      "last_updated_epoch": 1670886900,
-      "last_updated": "2022-12-12 20:15",
-      "temp_c": 0,
-      "temp_f": 0,
-      "is_day": 0,
-      "condition": {
-          "text": "Partly cloudy",
-          "icon": "//cdn.weatherapi.com/weather/64x64/night/116.png",
-          "code": 1003
-      },
-      "wind_mph": 4.3,
-      "wind_kph": 6.8,
-      "wind_degree": 20,
-      "wind_dir": "NNE",
-      "pressure_mb": 1010.0,
-      "pressure_in": 29.83,
-      "precip_mm": 0.5,
-      "precip_in": 0.02,
-      "humidity": 94,
-      "cloud": 75,
-      "feelslike_c": 21.0,
-      "feelslike_f": 69.8,
-      "vis_km": 10.0,
-      "vis_miles": 6.0,
-      "uv": 1.0,
-      "gust_mph": 6.5,
-      "gust_kph": 10.4
-  }
+  current: {
+    last_updated_epoch: 1670886900,
+    last_updated: '2022-12-12 20:15',
+    temp_c: 0,
+    temp_f: 0,
+    is_day: 0,
+    condition: {
+      text: 'Partly cloudy',
+      icon: '//cdn.weatherapi.com/weather/64x64/night/116.png',
+      code: 1003,
+    },
+    wind_mph: 4.3,
+    wind_kph: 6.8,
+    wind_degree: 20,
+    wind_dir: 'NNE',
+    pressure_mb: 1010.0,
+    pressure_in: 29.83,
+    precip_mm: 0.5,
+    precip_in: 0.02,
+    humidity: 94,
+    cloud: 75,
+    feelslike_c: 21.0,
+    feelslike_f: 69.8,
+    vis_km: 10.0,
+    vis_miles: 6.0,
+    uv: 1.0,
+    gust_mph: 6.5,
+    gust_kph: 10.4,
+  },
 }
 
 export default initialData
